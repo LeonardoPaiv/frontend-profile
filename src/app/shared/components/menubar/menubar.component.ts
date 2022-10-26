f3b55176f0925aa89f08a27efbf40f53e5d1ce90
leonardoPaiv <leopaiva45@gmail.com>
2022-10-25 22:10:16 -0300

    visual changes

--- src/app/shared/components/menubar/menubar.component.ts
+++ src/app/shared/components/menubar/menubar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menubar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenubarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public goHome() {
+    this.route.navigate([''])
   }
 
   public display: boolean = false;
