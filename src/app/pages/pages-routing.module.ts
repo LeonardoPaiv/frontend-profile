66001db80239b972cde9a7bf3d2d2517922bdb10
leonardoPaiv <leopaiva45@gmail.com>
2022-11-10 00:31:45 -0300

    pagina de login criada

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -8,8 +9,8 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'teste',
-    component: HomeComponent
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,18 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 import lottie from 'lottie-web';
 import { defineLordIconElement } from 'lord-icon-element';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
+
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
     SharedModule,
+    InputTextModule,
+    ButtonModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
